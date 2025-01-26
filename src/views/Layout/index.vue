650405d9c11ed561aab1d016448d4ac8fcf98be5
QiaQiadiao <1832411836@qq.com>
2025-01-26 15:34:00 +0800

    pinia优化导航数据

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,10 @@ import LayoutFixed from './commponents/LayoutFixed.vue';
 import LayoutNav from '@/views/Layout/commponents/LayoutNav.vue'
 import LayoutHeader from '@/views/Layout/commponents/LayoutHeader.vue'
 import LayoutFooter from '@/views/Layout/commponents/LayoutFooter.vue'
+import { useCategoryStore } from '@/stores/category';
+import { onMounted } from 'vue';
+const categoryStore = useCategoryStore();
+onMounted(()=>categoryStore.getCategory());
 </script>
 
 <template>
