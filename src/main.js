086c328d3020f4edfe3346bbbf4856d21f7224b4
QiaQiadiao <1832411836@qq.com>
2025-02-04 10:36:38 +0800

    home-图片懒加载指令实现

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { useIntersectionObserver } from '@vueuse/core'
 import App from './App.vue'
 import router from './router'
 import '@/styles/common.scss'
@@ -12,3 +12,17 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//自定义指令
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        useIntersectionObserver(
+            el,
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    el.src = binding.value
+                }
+            },
+        )
+    }
+})
