a0c19d6e3f03511b1edc4ac440c89079bbe2fa71
QiaQiadiao <1832411836@qq.com>
2025-02-12 10:36:29 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -7,3 +7,12 @@ export function getCategoryAPI(id) {
         }
     })
 }
+// 获取二级分类列表数据
+export const getCategoryFilterAPI = (id) => {
+    return request({
+        url: '/category/sub/filter',
+        params: {
+            id
+        }
+    })
+}
