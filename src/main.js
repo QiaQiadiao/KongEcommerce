ad210d494aa7eb23f0ae24d09321ffbfed8856af
QiaQiadiao <1832411836@qq.com>
2025-02-18 15:00:25 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import '@/styles/common.scss'
 //引入懒加载插件
 import { lazyPlugin } from './directives'
 import { componentPlugin } from './components'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)
--- src/stores/user.js
+++ src/stores/user.js
@@ -12,4 +12,4 @@ export const useUserStore = defineStore('user', () => {
         userInfo,
         getUserInfo
     }
-})
+}, { persist: true, })
