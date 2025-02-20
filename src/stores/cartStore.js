0fa145d0b4c4b01f536f608e3eb0a057db0228f2
QiaQiadiao <1832411836@qq.com>
2025-02-20 19:58:34 +0800

    本地购物车-添加购物车实现

---
+++ src/stores/cartStore.js
@@ -0,0 +1,21 @@
+//购物车模块
+import { defineStore } from "pinia";
+import { ref } from "vue";
+export const useCartStone = defineStore('cart', () => {
+    const cartList = ref([])
+    const addCart = (goods) => {
+        const item = cartList.value.find((item) => goods.skuId === item.skuId)
+        if (item) {
+            item.count++
+        } else {
+            cartList.value.push(goods)
+        }
+    }
+    return {
+        cartList,
+        addCart
+
+    }
+}, {
+    persist: true
+})
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,9 @@ import { getDetail } from '@/apis/detail';
 import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import DetailHot from './components/DetailHot.vue';
+import { ElMessage } from 'element-plus';
+import { useCartStone } from '@/stores/cartStore';
+const cartStore = useCartStone()
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -11,8 +14,30 @@ const getGoods = async () => {
   
 }
 onMounted(()=>getGoods())
+let skuObj = {}
 const skuChange = (sku) => {
-
+  skuObj = sku
+}
+const count = ref(1)
+const countChange = (count) => {
+  console.log(count);
+  
+}
+const addCart = () => {
+  if( skuObj.skuId) {
+    cartStore.addCart({
+      id: goods.value.id,
+      name: goods.value.name,
+      picture: goods.value.mainPictures[0],
+      price: goods.value.price,
+      count: count.value,
+      skuId: skuObj.skuId,
+      attrsText: skuObj.specsText,
+      selected: true
+    })
+  }else{
+    ElMessage.warning('请选择规格')
+  }
 }
 </script>
 
@@ -86,10 +111,10 @@ const skuChange = (sku) => {
               <!-- sku组件 -->
               <XtxSku :goods="goods" @change="skuChange"></XtxSku>
               <!-- 数据组件 -->
-
+              <el-input-number v-model="count" @change="countChange"></el-input-number>
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn">
+                <el-button size="large" class="btn" @click="addCart">
                   加入购物车
                 </el-button>
               </div>
