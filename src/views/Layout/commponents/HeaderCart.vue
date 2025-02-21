9b464954e3c0b3f925e6aa94544b2f1cbf0a6bb4
QiaQiadiao <1832411836@qq.com>
2025-02-21 10:40:25 +0800

    购物车-基础数据渲染

--- src/views/Layout/commponents/HeaderCart.vue
+++ src/views/Layout/commponents/HeaderCart.vue
@@ -33,7 +33,7 @@ const cartStone = useCartStone()
           <p>共 {{cartStone.allCount}} 件商品</p>
           <p>&yen; {{ cartStone.allPrice.toFixed(2) }} </p>
         </div>
-        <el-button size="large" type="primary" >去购物车结算</el-button>
+        <el-button size="large" type="primary" @click="$router.push('/cartlist')">去购物车结算</el-button>
       </div>
     </div>
 </div>
